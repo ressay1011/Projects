7fdd3e43eca7297a3c4dcaebd477019f962d90c7
Yasser Daniel Ariza Barrios <ressay1011@gmail.com>
2024-10-24 22:39:08 -0500

    Class 1 exercisises

---
+++ DevSenior/Ejercicios/Clase 01/PersonalGreeting.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PersonalGreeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String typedName = "";
+        String errorMessage = "";
+        boolean error = true;
+
+        while (error == true) {
+            System.out.print("Enter your name:");
+            typedName = input.nextLine();
+
+            typedName = typedName.strip();
+
+            if (typedName.isBlank()) {
+                errorMessage = "Please enter a name.";
+            } else if (!typedName.matches("[a-zA-Z]+")) {
+                errorMessage = "Please enter only letters in the name field.";
+            } else {
+                error = false;
+            }
+
+            if (error == true) {
+                System.err.printf("Error: %s%n", errorMessage);
+            }
+        }
+        typedName = typedName.replace(typedName.charAt(0), Character.toUpperCase(typedName.charAt(0)));
+        System.out.printf("Hello %s, welcome to the Java programming course!!", typedName);
+        input.close();
+    }
+}
---
+++ DevSenior/Ejercicios/Clase 01/PrintFace.java
@@ -0,0 +1,5 @@
+public class PrintFace {
+    public static void main(String[] args) {
+        System.out.print(" @@@@@@@ \n@| O O |@\n(|  ^  |)\n | [_] |\n +-----+");
+    }
+}
