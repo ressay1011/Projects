3ab5488de781a8123178baa642ef50a15f38acfb
Yasser Daniel Ariza Barrios <ressay1011@gmail.com>
2024-11-24 16:52:22 -0500

    feat: Destination planet selection implemented fix: Some menu errors fixed.

--- DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
+++ DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
@@ -15,14 +15,47 @@ public class Challengue01 {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        int option;
-        String[] options = {
-                "Select destination planet.",
-                "Select spacecraft.",
-                "Start the travel simulation.",
-                "Exit the program." };
-        option = showMenu(options);
-        menu(option);
+        while (true) {
+            int option;
+            int destinationPlanet;
+            String[] planets = { "Mercury", "Venus", "Mars", "Jupiter", "Saturn",
+                    "Uranus", "Neptune" };
+            String[] options = {
+                    "Select destination planet",
+                    "Select spacecraft",
+                    "Start the travel simulation",
+                    "Exit the program" };
+            option = showMenu(options);
+
+            switch (option) {
+                case 0:
+                    System.out.printf("%1$sExiting the program...%2$s", BLUE, WHITE);
+                    System.exit(0);
+                    break;
+                case 1:
+                    options = new String[(planets.length + 1)];
+
+                    for (int i = 0; i < planets.length; i++) {
+                        options[i] = planets[i];
+                    }
+
+                    options[planets.length] = "Back";
+                    option = showMenu(options);
+
+                    if (option != 0) {
+                        destinationPlanet = option;
+                        menuDestinationPlanet(destinationPlanet, planets);
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Como rayos llegaste aqui?");
+                    break;
+            }
+        }
     }
 
     private static int showMenu(String[] options) {
@@ -51,10 +84,10 @@ public class Challengue01 {
 
             for (String option : options) {
                 if (i != (nOptions - 1)) {
-                    optionsTemp = optionsTemp.concat("%1$s" + (i + 1) + ".%2$s " + option + "\n");
+                    optionsTemp = optionsTemp.concat("%1$s" + (i + 1) + ".%2$s " + option + ".\n");
                 } else {
                     optionsTemp = optionsTemp.concat("%1$s" + (i - (nOptions - 1)) + ".%2$s "
-                            + option + "\n");
+                            + option + ".\n");
                 }
                 i = i + 1;
             }
@@ -84,85 +117,22 @@ public class Challengue01 {
         return selection;
     }
 
-    private static void menu(int option) {
-        String[] planets = { "Mercury.", "Venus.", "Mars.", "Jupiter.", "Saturn.",
-                "Uranus.", "Neptune." };
+    private static void menuDestinationPlanet(int destinationPlanet, String[] planets) {
         int[] planetDistance = { 91, 41, 225, 778, 1429, 2900, 4300 };
+        String[] planetDescription = {
+                "is the smallest and closest planet to the Sun.\nIt has a rocky surface with extreme temperature changes, ranging from -180°C at night to 430°C during the day.\nIt orbits the Sun in just 88 days.",
+                "is the second planet from the Sun and similar in size to Earth.\nIt has a thick, toxic atmosphere primarily made of carbon dioxide, which creates a runaway greenhouse effect, making it the hottest planet in the solar system.\nSurface temperatures can reach up to 470°C.\nVenus has no moons and rotates in the opposite direction to most planets, with a day longer than its year.",
+                "is the fourth planet from the Sun, known as the \"Red Planet\" due to its reddish appearance caused by iron oxide on its surface.",
+                "is the largest planet in the solar system, known for its massive size, strong magnetic field, and distinctive Great Red Spot, a giant storm.\nIt is a gas giant with no solid surface.",
+                "is the second-largest planet in the solar system, famous for its stunning ring system made of ice and rock.\nIt is a gas giant with a predominantly hydrogen and helium atmosphere.",
+                "is the seventh planet from the Sun and is known for its distinctive blue-green color, caused by methane in its atmosphere.\nIt is an ice giant with a mostly hydrogen and helium atmosphere, but it also contains water, ammonia, and methane.\nUranus has a unique rotation, spinning on its side with an axial tilt of 98 degrees.\nThis results in extreme seasonal variations.\nIt has 13 rings and 27 known moons.",
+                "is the eighth and farthest planet from the Sun.\nIt is an ice giant with a deep blue color due to methane in its atmosphere.\nNeptune has strong winds, the fastest in the solar system, and large storm systems, including the Great Dark Spot.\nIt has 14 known moons, with Triton being the largest, and five rings.\nIts atmosphere is mostly hydrogen, helium, and methane."
+        };
 
-        switch (option) {
-            case 0:
-                System.out.printf("%1$sExiting the program...%2$s", BLUE, WHITE);
-                System.exit(0);
-                break;
-            case 1:
-                String[] options = new String[(planets.length + 1)];
-                for (int i = 0; i < planets.length; i++) {
-                    options[i] = planets[i];
-                }
-                options[planets.length] = "Back.";
-                option = showMenu(options);
-                menuDestinationPlanet(option);
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                System.out.println("Como rayos llegaste aqui?");
-                break;
-        }
-    }
-
-    private static void menuDestinationPlanet(int option) {
-
+        System.out.printf(
+                "The selected planet is: %1$s located approximately %2$d million kilometers from Earth.\nHere is a brief description: %3$s\nPress enter to continue.",
+                planets[destinationPlanet - 1], planetDistance[destinationPlanet - 1],
+                planetDescription[destinationPlanet - 1]);
+        input.nextLine();
     }
 }
-
-/*
- * private static void menu() {
- * 
- * private static int menuDestinationPlanet(String[] planets, int[]
- * planetDistance) {
- * int destinationPlanet;
- * String[] planetDescription = {
- * "Mars is the fourth planet from the Sun, known as the \"Red Planet\" due to its reddish appearance caused by iron oxide on its surface."
- * ,
- * "Jupiter is the largest planet in the solar system, known for its massive size, strong magnetic field, and distinctive Great Red Spot, a giant storm. It is a gas giant with no solid surface."
- * ,
- * "Saturn is the second-largest planet in the solar system, famous for its stunning ring system made of ice and rock. It is a gas giant with a predominantly hydrogen and helium atmosphere."
- * };
- * 
- * while (error == true) {
- * logo();
- * System.out.printf("%1$s1.%2$s Marte\n"
- * + "%1$s2.%2$s Júpiter\n"
- * + "%1$s3.%2$s Saturno\n"
- * + "%1$s0.%2$s Back\n", GREEN, WHITE);
- * System.out.printf("%1$sPlease select the destination planet (0 - 3):%2$s ",
- * BLUE, WHITE);
- * 
- * if (input.hasNextInt()) {
- * option = input.nextInt();
- * input.nextLine();
- * switch (option) {
- * case 0:
- * menu();
- * break;
- * case 1:
- * 
- * break;
- * default:
- * System.out.printf("%1$sError: The option %3$d is not valid.%2$s\n", RED,
- * WHITE, option);
- * break;
- * }
- * } else {
- * System.out.printf("%1$sError: Please enter a number from 0 to 3.%2$s\n", RED,
- * WHITE);
- * input.nextLine();
- * }
- * }
- * 
- * return destinationPlanet = option;
- * }
- */
