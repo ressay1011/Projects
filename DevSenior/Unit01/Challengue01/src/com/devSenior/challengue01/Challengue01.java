f9c5483f212982a81c493e11d29b810f65a82e35
Yasser Daniel Ariza Barrios <ressay1011@gmail.com>
2024-11-25 02:28:27 -0500

    fix: unused variables

--- DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
+++ DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
@@ -6,10 +6,7 @@ public class Challengue01 {
 
     static final String RED = "\u001B[31m";
     static final String GREEN = "\u001B[32m";
-    // static final String YELLOW = "\u001B[33m";
     static final String BLUE = "\u001B[34m";
-    // static final String PURPLE = "\u001B[35m";
-    // static final String CYAN = "\u001B[36m";
     static final String WHITE = "\u001B[37m";
 
     static Scanner input = new Scanner(System.in);
