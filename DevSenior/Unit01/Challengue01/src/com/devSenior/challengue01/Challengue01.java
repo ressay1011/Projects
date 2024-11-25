e6bf54c8cf50ea4809eac897cc2c6ed70a68744e
Yasser Daniel Ariza Barrios <ressay1011@gmail.com>
2024-11-25 01:59:08 -0500

    fix: Some organization code issues

--- DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
+++ DevSenior/Unit01/Challengue01/src/com/devSenior/challengue01/Challengue01.java
@@ -17,14 +17,13 @@ public class Challengue01 {
     public static void main(String[] args) throws Exception {
         while (true) {
             int option;
+
             String[] planets = { "Mercury", "Venus", "Mars", "Jupiter", "Saturn",
                     "Uranus", "Neptune" };
-            int[] planetDistance = { 91, 41, 225, 778, 1429, 2900, 4300 };
-            int destinationPlanet;
+            String[] destinationPlanet;
+
             String[] spaceShips = { "Red dwarf", "Discovery", "Millennium falcon" };
-            float[] spaceShipsMaxVel = {};
-            int[] spaceShipsMaxCapacity = {};
-            int spaceShipsCapacity;
+            String[] selectedSpaceShip;
 
             String[] options = {
                     "Select destination planet",
@@ -41,7 +40,7 @@ public class Challengue01 {
                 case 1:
                     options = new String[(planets.length + 1)];
 
-                    for (int i = 0; i < planets.length; i++) {
+                    for (var i = 0; i < planets.length; i++) {
                         options[i] = planets[i];
                     }
 
@@ -49,11 +48,22 @@ public class Challengue01 {
                     option = showMenu(options);
 
                     if (option != 0) {
-                        destinationPlanet = option;
-                        menuDestinationPlanet(destinationPlanet, planets, planetDistance);
+                        destinationPlanet = destinationPlanet(option, planets);
                     }
                     break;
                 case 2:
+                    options = new String[(spaceShips.length + 1)];
+
+                    for (var i = 0; i < spaceShips.length; i++) {
+                        options[i] = spaceShips[i];
+                    }
+
+                    options[spaceShips.length] = "Back";
+                    option = showMenu(options);
+
+                    if (option != 0) {
+                        selectedSpaceShip = selectedSpaceShip(option, spaceShips);
+                    }
                     break;
                 case 3:
                     break;
@@ -65,13 +75,13 @@ public class Challengue01 {
     }
 
     private static int showMenu(String[] options) {
-        String optionsTemp;
-        int nOptions = options.length;
-        int i;
-        int selectionTemp;
-        int selection = 0;
-        boolean error = true;
-        String errorMsg = "";
+        var optionsTemp = "";
+        var nOptions = options.length;
+        var i = 0;
+        var selectionTemp = 0;
+        var selection = 0;
+        var error = true;
+        var errorMsg = "";
 
         while (error == true) {
             optionsTemp = "";
@@ -123,7 +133,8 @@ public class Challengue01 {
         return selection;
     }
 
-    private static void menuDestinationPlanet(int destinationPlanet, String[] planets, int[] planetDistance) {
+    private static String[] destinationPlanet(int option, String[] planets) {
+        String[] planetDistance = { "91", "41", "225", "778", "1429", "2900", "4300" };
         String[] planetDescription = {
                 "is the smallest and closest planet to the Sun.\nIt has a rocky surface with extreme temperature changes, ranging from -180°C at night to 430°C during the day.\nIt orbits the Sun in just 88 days.",
                 "is the second planet from the Sun and similar in size to Earth.\nIt has a thick, toxic atmosphere primarily made of carbon dioxide, which creates a runaway greenhouse effect, making it the hottest planet in the solar system.\nSurface temperatures can reach up to 470°C.\nVenus has no moons and rotates in the opposite direction to most planets, with a day longer than its year.",
@@ -133,11 +144,42 @@ public class Challengue01 {
                 "is the seventh planet from the Sun and is known for its distinctive blue-green color, caused by methane in its atmosphere.\nIt is an ice giant with a mostly hydrogen and helium atmosphere, but it also contains water, ammonia, and methane.\nUranus has a unique rotation, spinning on its side with an axial tilt of 98 degrees.\nThis results in extreme seasonal variations.\nIt has 13 rings and 27 known moons.",
                 "is the eighth and farthest planet from the Sun.\nIt is an ice giant with a deep blue color due to methane in its atmosphere.\nNeptune has strong winds, the fastest in the solar system, and large storm systems, including the Great Dark Spot.\nIt has 14 known moons, with Triton being the largest, and five rings.\nIts atmosphere is mostly hydrogen, helium, and methane."
         };
+        String[] destinationPlanetInfo = new String[2];
+        destinationPlanetInfo[0] = planets[option - 1];
+        destinationPlanetInfo[1] = planetDistance[option - 1];
 
         System.out.printf(
-                "%4$sThe selected planet is:%6$s %1$s located approximately %2$d million kilometers from Earth.\nHere is a brief description: %3$s\n%5$sPress enter to continue.%6$s",
-                planets[destinationPlanet - 1], planetDistance[destinationPlanet - 1],
-                planetDescription[destinationPlanet - 1], GREEN, BLUE, WHITE);
+                "%4$sThe selected planet is:%6$s %1$s located approximately %2$s million kilometers from Earth %3$s\n%5$sPress enter to continue.%6$s",
+                destinationPlanetInfo[0], destinationPlanetInfo[1],
+                planetDescription[option - 1], GREEN, BLUE, WHITE);
         input.nextLine();
+
+        return destinationPlanetInfo;
     }
+
+    private static String[] selectedSpaceShip(int option, String[] spaceShips) {
+        String[] spaceShipsMaxVel = { "36.4", "28", "42" };
+        String[] spaceShipsMaxCapacity = { "10", "25", "5" };
+        String[] spaceShipsMaxFuel = { "600", "900", "450" };
+        String[] spaceShipsMaxOxygen = { "600", "900", "450" };
+        String[] spaceShipsMaxWeight = { "3000", "5050", "2000" };
+        var spaceShipsCapacity = "";
+
+        String[] selectedSpaceShipInfo = new String[6];
+        selectedSpaceShipInfo[0] = spaceShipsMaxVel[option - 1];
+        selectedSpaceShipInfo[1] = spaceShipsMaxCapacity[option - 1];
+        selectedSpaceShipInfo[2] = spaceShipsMaxFuel[option - 1];
+        selectedSpaceShipInfo[3] = spaceShipsMaxOxygen[option - 1];
+        selectedSpaceShipInfo[4] = spaceShipsMaxWeight[option - 1];
+        selectedSpaceShipInfo[5] = spaceShipsCapacity;
+
+        System.out.printf(
+                "%4$sThe selected planet is:%6$s %1$s located approximately %2$s million kilometers from Earth %3$s\n%5$sPress enter to continue.%6$s",
+                destinationPlanetInfo[0], destinationPlanetInfo[1],
+                planetDescription[option - 1], GREEN, BLUE, WHITE);
+        input.nextLine();
+
+        return selectedSpaceShipInfo;
+    }
+
 }
